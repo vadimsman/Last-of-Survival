2b97eec19b7b47866e65d5c574ddc2d35fe631b1
vadimsman <159170164+vadimsman@users.noreply.github.com>
2024-04-12 15:38:46 +0300

    0.1.2 Pre-Alpha
    Добавил кнопку выхода из игры, кнопку титров, кнопку возвращения в меню из титров.

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,8 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
+    public GameObject ButtonUI;
+    public GameObject TiterUI;
+    private void Start()
+    {
+        TiterUI.SetActive(false);
+    }
     public void PlayButton(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Titer()
+    {
+        ButtonUI.SetActive(false);
+        TiterUI.SetActive(true);
+    }
+
+    public void ExitTiter()
+    {
+        ButtonUI.SetActive(true);
+        TiterUI.SetActive(false);
     }
 }
