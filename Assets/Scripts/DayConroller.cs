1135cff7a33f8c1c01d1cd396ace1accfce948bc
vadimsman <159170164+vadimsman@users.noreply.github.com>
2024-04-14 19:40:40 +0300

    0.2.7
    Сделал смену дня и ночи(с багом который в ближайшее время исправлю)

---
+++ Assets/Scripts/DayConroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayConroller : MonoBehaviour
+{
+    public Transform DirectionLight;
+
+    public float Speed;
+
+    public float DayXRotate;
+
+    public float NightXRotate;
+
+    public bool DayOrNight; //День это true, ночь это false
+
+    public GameObject LightGroup;
+
+    private float _newAngleX;
+
+    public void Update()
+    {
+        _newAngleX = DirectionLight.localEulerAngles.x - Time.deltaTime * Speed * 1;
+        DirectionLight.localEulerAngles = new Vector3(_newAngleX, 0, 0);
+        DayUpdate();
+    }
+
+    public void DayUpdate()
+    {
+        if(_newAngleX <= NightXRotate)
+        {
+            LightGroup.SetActive(true);
+            DayOrNight = false;
+        }
+
+        if(_newAngleX <= DayXRotate)
+        {
+            LightGroup.SetActive(false);
+            _newAngleX = 50;
+            DayOrNight = true;
+        }
+    }
+}
