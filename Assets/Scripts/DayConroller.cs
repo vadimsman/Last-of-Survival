3fb43aaf727a85b6c3275f59de14e99680f07ffa
vadimsman <159170164+vadimsman@users.noreply.github.com>
2024-04-15 16:28:32 +0300

    0.2.9
    Починил день и ночь.

--- Assets/Scripts/DayConroller.cs
+++ Assets/Scripts/DayConroller.cs
@@ -4,44 +4,41 @@ using UnityEngine;
 
 public class DayConroller : MonoBehaviour
 {
-    public Transform DirectionLight;
-
     public float Speed;
 
     public float DayXRotate;
 
     public float NightXRotate;
 
-    public bool DayOrNight; //День это true, ночь это false
+    public bool DayOrNight = true; //День это true, ночь это false
 
     public GameObject LightGroup;
 
     private float _newAngleX;
 
+    private bool _isDay;
+
     public void Start()
     {
-        
+        _newAngleX = transform.eulerAngles.x;
     }
 
     public void Update()
     {
-        DirectionLight.transform.Rotate(Vector3.right * Speed * Time.deltaTime);
-        DayUpdate();
+        transform.Rotate(Vector3.right * Speed * Time.deltaTime);
+        _newAngleX = transform.eulerAngles.x;
+        _isDay = _newAngleX > DayXRotate && _newAngleX < NightXRotate;
     }
 
-    public void DayUpdate()
+    private void LightOnOrOf()
     {
-        if(_newAngleX <= NightXRotate)
-        {
-            LightGroup.SetActive(true);
-            DayOrNight = false;
-        }
-
-        if(_newAngleX <= DayXRotate)
+        if (_isDay)
         {
             LightGroup.SetActive(false);
-            _newAngleX = 50;
-            DayOrNight = true;
+        }
+        if (!_isDay)
+        {
+            LightGroup.SetActive(enabled);
         }
     }
 }
