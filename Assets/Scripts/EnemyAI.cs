c20cc9f67395973a41f137b2f421087dddfa7b09
ivan290210 <bublan28@yandex.ru>
2024-04-17 22:11:16 +0300

    0.3.3
    Сделал прогрессию в поведение моба, чтобы сначала он ломал стену, а потом патрулировал по точкам, но есть баг, который позже исправлю

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,18 +6,50 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent _mob;
     [SerializeField] private List<Transform> _point;
+    
+    [SerializeField] private GatesHealth _gatesHealth;
+
+    [SerializeField] private float _amount = 10;
 
     private void Start()
     {
         _mob = GetComponent<NavMeshAgent>();
-        _mob.destination = _point[Random.Range(0, _point.Count)].position;
+        
     }
 
     private void Update()
     {
-        if (_mob.remainingDistance == 0)
+        var GatesDirection = _gatesHealth.transform.position - transform.position;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, GatesDirection, out hit))
+        {
+            if (hit.collider.gameObject == _gatesHealth.gameObject)
+            {
+                _mob.destination = _gatesHealth.transform.position;
+                
+                if (_mob.remainingDistance < _mob.stoppingDistance)
+                {
+                    _gatesHealth.DealDamage(_amount * Time.deltaTime);
+                }
+                
+            }
+        }
+
+        if (_gatesHealth.isGatesDestroy)
         {
             _mob.destination = _point[Random.Range(0, _point.Count)].position;
+            
+            if (_mob.remainingDistance < _mob.stoppingDistance && _gatesHealth._value <= 0)
+            {
+                _mob.destination = _point[Random.Range(0, _point.Count)].position;
+            }
         }
+        
+        
+
+        
     }
+    
+    
 }
---
+++ Assets/Scripts/GatesHealth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatesHealth : MonoBehaviour
+{ 
+    public float _value = 100;
+    public bool isGatesDestroy;
+
+    public void DealDamage(float amount)
+    {
+        isGatesDestroy = false;
+        _value -= amount;
+
+        if (_value <= 0)
+        {
+            Destroy(gameObject);
+            isGatesDestroy = true;
+        }
+    }
+}
+ 
