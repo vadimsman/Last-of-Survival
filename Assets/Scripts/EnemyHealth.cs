dd89c794c99852806ca4d18e7561d99dd8ad63b6
vadimsman <159170164+vadimsman@users.noreply.github.com>
2024-04-19 18:25:32 +0300

    0.3.4
    Начал делать скрипт стрельбы, позже доделаю

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value;
+    
+    public void Damage(float DamageValue)
+    {
+        value -= DamageValue;
+        Death();
+    }
+
+    public void Death()
+    {
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+    public float BasicWeaponDamage;
+    public GameObject EnemyPrefabs;
+    public List<GameObject> unityGameObjects = new List<GameObject>();
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
