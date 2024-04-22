3830a9e986fe9febdb4c64215d3db148bedbefb0
ivan290210 <bublan28@yandex.ru>
2024-04-22 16:57:10 +0300

    0.3.6
    Сделал основной скрипт перемещения моба, доделал логику скрипта с ломанием забора. Так же сделал отключение 1 скрипта и включение основного

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,55 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent _mob;
-    [SerializeField] private List<Transform> _point;
-    
-    [SerializeField] private GatesHealth _gatesHealth;
-
-    [SerializeField] private float _amount = 10;
+    private NavMeshAgent _mob;
+    public List<Transform> Point;
+    private EnemyGatesEvent _enemyGatesEvent;
 
     private void Start()
     {
-        _mob = GetComponent<NavMeshAgent>();
-        
+        LinksGetComponents();
+        RandomPatrolPoints();
+        _enemyGatesEvent.enabled = false;
     }
 
     private void Update()
     {
-        var GatesDirection = _gatesHealth.transform.position - transform.position;
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, GatesDirection, out hit))
-        {
-            if (hit.collider.gameObject == _gatesHealth.gameObject)
-            {
-                _mob.destination = _gatesHealth.transform.position;
-                
-                if (_mob.remainingDistance < _mob.stoppingDistance)
-                {
-                    _gatesHealth.DealDamage(_amount * Time.deltaTime);
-                }
-                
-            }
-        }
-
-        if (_gatesHealth.isGatesDestroy)
-        {
-            _mob.destination = _point[Random.Range(0, _point.Count)].position;
-            
-            if (_mob.remainingDistance < _mob.stoppingDistance && _gatesHealth._value <= 0)
-            {
-                _mob.destination = _point[Random.Range(0, _point.Count)].position;
-            }
-        }
-        
-        
-
-        
+        CheckingDistancePoints();
     }
-    
-    
+
+    private void LinksGetComponents()
+    {
+        _mob = GetComponent<NavMeshAgent>();
+        _enemyGatesEvent = GetComponent<EnemyGatesEvent>();
+    }
+
+    private void RandomPatrolPoints()
+    {
+        _mob.destination = Point[Random.Range(0, Point.Count)].position;
+    }
+
+    private void CheckingDistancePoints()
+    {
+        if (_mob.remainingDistance < _mob.stoppingDistance)
+        {
+            RandomPatrolPoints();
+        }
+    }
 }
---
+++ Assets/Scripts/EnemyGatesEvent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyGatesEvent : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _mob;
+
+    [SerializeField] private GatesHealth _gatesHealth;
+
+    [SerializeField] private float _amount = 10;
+
+    private EnemyAI _mobPatrol;
+
+    private void Start()
+    {
+        LinkGetComponents();
+    }
+
+    private void Update()
+    {
+        GateDetectionAndDamage();
+
+        EnablingTheMainScript();
+
+    }
+
+    private void GateDetectionAndDamage()
+    {
+        var GatesDirection = _gatesHealth.transform.position - transform.position;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, GatesDirection, out hit))
+        {
+            if (hit.collider.gameObject == _gatesHealth.gameObject)
+            {
+                _mob.destination = _gatesHealth.transform.position;
+                
+                if (_mob.remainingDistance < _mob.stoppingDistance)
+                {
+                    _gatesHealth.DealDamage(_amount * Time.deltaTime);
+                }
+                
+            }
+        }
+    }
+
+    private void EnablingTheMainScript()
+    {
+        if (_gatesHealth.isGatesDestroy)
+        {
+            _mobPatrol.enabled = true;
+        }
+    }
+
+    private void LinkGetComponents()
+    {
+        _mob = GetComponent<NavMeshAgent>();
+        _mobPatrol = GetComponent<EnemyAI>();
+    }
+}
