36d20dc670ec3e43a4a5a7278bc031413ff116b3
ivan290210 <bublan28@yandex.ru>
2024-04-24 03:15:12 +0300

    0.3.9
    Начал делать инвентарь(не доделано!!!!!)

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject UIPanel;
+    public Transform inventoryPanel;
+    public List<InventorySlot> slots = new List<InventorySlot>();
+    private bool isOpened;
+    void Start()
+    {
+        for (int i = 0; i < inventoryPanel.childCount; i++)
+        {
+            if (inventoryPanel.GetChild(i).GetComponent<InventorySlot>() != null)
+            {
+                 slots.Add(inventoryPanel.GetChild(i).GetComponent<InventorySlot>());
+            }
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isOpened = !isOpened;
+            
+            if (isOpened)
+            {
+                UIPanel.SetActive(true);
+            }
+            else
+            {
+                UIPanel.SetActive(false);
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Inventory/ItemScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType {Default, Food, Weapon, Instrument}
+public class ItemScriptableObject : ScriptableObject
+{
+    public ItemType itemType;
+    public string itemName;
+    public GameObject itemPrefab;
+    public int maxAmount;
+    public string itemDescription;
+}
