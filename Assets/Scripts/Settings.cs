9d8e8ef3f57d0061b70c1d7952740c37b6d40b7f
vadimsman <vadimsman@mail.ru>
2024-04-25 14:25:02 +0300

    0.4.1
    Начал делать настройки графики(не доделал)

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    void Start()
+    public int Quality;
+
+    public void setQuality(int _quality)
     {
-        
+        Quality = _quality;
+    }
+
+    public void confirmSettings()
+    {
+        QualitySettings.SetQualityLevel(Quality);
     }
 }
---
+++ Assets/Scripts/SettingsTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log(QualitySettings.names);
+            QualitySettings.SetQualityLevel(0, false);
+            QualitySettings.SetQualityLevel(4, true);
+        }
+    }
+}
