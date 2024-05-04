7b3c89448fd110271da2be47aceea5706b46d6e1
vadimsman <vadimsman@mail.ru>
2024-05-04 20:18:13 +0300

    0.4.4
    Начал делать стрельбу(не доделал)

--- Assets/Scripts/WeaponShoot.cs
+++ Assets/Scripts/WeaponShoot.cs
@@ -4,22 +4,55 @@ using UnityEngine;
 
 public class WeaponShoot : MonoBehaviour
 {
-    public float BasicWeaponDamage;
+    public float Damage;
     public Camera CameraLink;
-    public GameObject EnemyPrefabs;
-    public List<GameObject> unityGameObjects = new List<GameObject>();
+    public float KeyDown;
+    public float KeyDownSpeed;
+    public int Ammo;
+    public int MaxAmmo;
+    public int AmmoInInventory;
+    public float ReloadTime;
+
+    private int NeedAmmo;
+    private float _currentReloadTime;
 
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        KeyDown -= KeyDownSpeed;
+        if (KeyDown < 0)
         {
-            var ray = CameraLink.ViewportPointToRay(new Vector3(0, 0, 0));
-
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
-            {
-                
-            }
+            KeyDown = 0;
         }
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = CameraLink.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (KeyDown == 0 && Ammo != 0)
+                {
+                    Ammo -= 1;
+                    if (hit.collider.gameObject.GetComponent<EnemyHealth>() != null)
+                    {
+                        GetComponent<EnemyHealth>().Damage(Damage);
+                    }
+                    KeyDown = 1;
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    private void Reload()
+    {
+        NeedAmmo = MaxAmmo - Ammo;
+        AmmoInInventory -= NeedAmmo;
+
+        Ammo = MaxAmmo;
+        _currentReloadTime = ReloadTime;
     }
 }
