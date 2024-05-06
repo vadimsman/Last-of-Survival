528b242d54899e42fdc6db7ee1086ed4288a054c
vadimsman <vadimsman@mail.ru>
2024-05-06 15:26:40 +0300

    0.4.5
    Начал делать систему интерьера для домов(не доделал)!! Помощь нужна!!

---
+++ Assets/Scripts/BuildingEnter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BuildingEnter : MonoBehaviour
+{
+    public string NameScene;
+    public GameObject EnterUI;
+    public GameObject GamePlayUI;
+
+    private bool _isEnterUIOpen;
+
+    public void Update()
+    {
+        if (_isEnterUIOpen == true && Input.GetKeyDown(KeyCode.F))
+        {
+            GoToScene();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other = GetComponent<CharacterController>())
+        {
+            EnterUI.SetActive(true);
+            GamePlayUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            _isEnterUIOpen = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other = GetComponent<CharacterController>())
+        {
+            EnterUI.SetActive(false);
+            GamePlayUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            _isEnterUIOpen = false;
+        }
+    }
+
+    public void GoToScene()
+    {
+        SceneManager.LoadScene(NameScene);
+    }
+}
