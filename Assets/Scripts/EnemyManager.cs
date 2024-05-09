2673a00d7f7b11d4809ccc9661bb7afd13221d67
vadimsman <vadimsman@mail.ru>
2024-05-09 19:29:21 +0300

    0.4.6
    Переделал полностью стрельбу(не доделал) в ближайшее время доделаю стрельбу и начну заниматься интерьером

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int targetLayer = 8; // Установите номер слоя маски, который вы хотите использовать для врагов
+
+    void Start()
+    {
+        // Получаем все враги в сцене
+        GameObject[] enemies = FindObjectsOfType<GameObject>();
+
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy.CompareTag("Enemy")) // Проверяем, имеет ли объект тег "Enemy"
+            {
+                // Добавляем врага на слой маски targetLayer
+                enemy.layer = targetLayer;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public LayerMask targetLayer; // Маска слоя для определения целей
+    public Transform muzzlePosition; // Позиция ствола оружия
+    public Transform firePoint; // Точка, откуда производится выстрел
+    public float force = 10f; // Сила выстрела
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Проверка нажатия кнопки огня
+        {
+            RaycastHit hit;
+            // Создание луча от точки стрельбы в направлении взгляда камеры
+            if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, Mathf.Infinity, targetLayer))
+            {
+                // Если луч попал в цель
+                Debug.Log("Попал во что-то!");
+            }
+            else
+            {
+                // Если луч ни во что не попал
+                Debug.Log("Missed!");
+            }
+        }
+    }
+}
+
