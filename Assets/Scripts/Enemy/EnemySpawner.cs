29a25a3cf314d2418d01fb560ceb56cb55f7645d
vadimsman <vadimsman@mail.ru>
2024-05-16 19:41:37 +0300

    0.4.8.1
    Микро обнова начал делать скрипт спавнера врагов(написал одну строчку ибо появилось срочное дело)

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform EnemySpawnerObject;
+    public GameObject EnemyPrefab;
+
+    public void SpawnEnemy()
+    {
+        Instantiate(EnemyPrefab, EnemySpawnerObject.position, EnemySpawnerObject.rotation);
+    }
+}
