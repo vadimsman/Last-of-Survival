79e545d314d597cc75e1b18838ea5670f7eb8da0
vadimsman <vadimsman@mail.ru>
2024-05-30 09:18:44 +0300

    0.7.6

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource ShootSound;
+
+    public void ShootSoundPlay()
+    {
+        ShootSound.Play();
+    }
+}
--- Assets/Scripts/Player/ShootingScript.cs
+++ Assets/Scripts/Player/ShootingScript.cs
@@ -5,13 +5,14 @@ public class ShootingScript : MonoBehaviour
     public Transform muzzlePosition; // Позиция ствола оружия
     public Transform firePoint; // Точка, откуда производится выстрел
     public float damage;
+    public AudioController AudioController;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Проверка нажатия кнопки огня
         {
             RaycastHit hit;
-
+            AudioController.ShootSoundPlay();
             if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, Mathf.Infinity))
             {
                 if (hit.collider.GetComponent<EnemyHealth>() != null)
@@ -19,10 +20,6 @@ public class ShootingScript : MonoBehaviour
                     Debug.Log("POPAL");
                     var enemyHealth = hit.collider.GetComponent<EnemyHealth>();
                     enemyHealth.DamageUpdate(damage);
-                }
-                else
-                {
-                    Debug.Log("LOXSuk");
                 }
             }
         }
