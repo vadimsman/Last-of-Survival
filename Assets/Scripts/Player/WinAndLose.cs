8f0594b2e9621257cbc1fd87c5fce60e773e6e98
vadimsman <vadimsman@mail.ru>
2024-05-30 20:59:04 +0300

    1.0 BETA
    УРА БЕТА

--- Assets/Scripts/Player/WinAndLose.cs
+++ Assets/Scripts/Player/WinAndLose.cs
@@ -28,15 +28,16 @@ public class WinAndLose : MonoBehaviour
             Time.timeScale = 0;
             if (Input.GetKey(KeyCode.Escape))
             {
+                Time.timeScale = 1;
                 SceneManager.LoadScene("MainMenu");
             }
         }
 
-        if (_dayController.DayCount == 3 && _enemySpawner._enemies.Count == 0)
+        if (_dayController.DayCount == 5 && _enemySpawner._enemies.Count == 0)
         {
             WinUI.SetActive(true);
             GamePlayUI.SetActive(false);
-            Time.timeScale = 0;
+            Time.timeScale = 1;
             if (Input.GetKey(KeyCode.Escape))
             {
                 SceneManager.LoadScene("MainMenu");
